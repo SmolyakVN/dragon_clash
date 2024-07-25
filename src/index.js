e5a21e4daadd0e8dfc96ae369042eff15d6230e0
Vasily Smolyak <smolyak.vn@yandex.ru>
2024-07-25 03:15:42 +0300

    disabled router

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate
-} from "react-router-dom";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Navigate
+// } from "react-router-dom";
 import './index.css';
 import App from './App';
 
 // localStorage.setItem('userId', '');
 
-const router = createBrowserRouter([
-  {
-    path: "",
-    element: localStorage.getItem('userId') !== '' ? <App /> : <Navigate to="/authorization" />,
-  },
-  {
-    path: "/",
-    element: localStorage.getItem('userId') !== '' ? <App /> : <Navigate to="/authorization" />,
-  },
-  {
-    path: "/authorization",
-    element: localStorage.getItem('userId') === '' ? <App /> : <Navigate to="/" />,
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "",
+//     element: localStorage.getItem('userId') !== '' ? <App /> : <Navigate to="/authorization" />,
+//   },
+//   {
+//     path: "/",
+//     element: localStorage.getItem('userId') !== '' ? <App /> : <Navigate to="/authorization" />,
+//   },
+//   {
+//     path: "/authorization",
+//     element: localStorage.getItem('userId') === '' ? <App /> : <Navigate to="/" />,
+//   }
+// ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router} />
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <RouterProvider router={router} />
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
